4e117f417abd6a5a453ed6f5e364c8f3c6b6286a
Aidan McCarthy <48805366+amccarthy1813@users.noreply.github.com>
2023-07-17 22:39:29 -0400

    adjusted assets/index.js

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,5 @@
 /*Expected JSON object: See resume_schema.json*/
+import data from './resume.json' assert { type: 'json'};
+console.log(data);
 
-//var json = 
-// fetch('./resume.json')
-//             .then((response) => response.json())
-//             .then((json) => console.log(json));
- import data from './resume.json' assert { type: 'json'};
- console.log(data);
-//var resume = JSON.parse(json);
+
