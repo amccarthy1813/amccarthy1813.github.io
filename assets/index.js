1d490a9543910e6e8d5e7ade1576b0dffb87ee90
Aidan McCarthy <48805366+amccarthy1813@users.noreply.github.com>
2023-07-17 22:20:52 -0400

    commit new json and structure

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+/*Expected JSON object: See resume_schema.json*/
+
+//var json = 
+// fetch('./resume.json')
+//             .then((response) => response.json())
+//             .then((json) => console.log(json));
+ import data from './resume.json' assert { type: 'json'};
+ console.log(data);
+//var resume = JSON.parse(json);
